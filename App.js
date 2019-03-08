04d1186db2a5268375150976d055d5545498bce7
alejandro <alejandro.perez@ironbit.com.mx>
2019-03-08 12:31:03 -0600

    Removed stuff

--- App.js
+++ App.js
@@ -9,11 +9,15 @@ export default class App extends React.Component {
         <TitleBar>
           <Title>Welcome back</Title>
           <Name>Alex</Name>
+          <Avatar source={require("./assets/avatar.jpg")} />
+          <Subtitle>{"Continue Learning".toUpperCase()}</Subtitle>
         </TitleBar>
       </Container>
     );
   }
 }
+
+// Containers
 const Container = styled.View`
   background: #f0f3f5;
   flex: 1;
@@ -25,14 +29,34 @@ const TitleBar = styled.View`
   padding-left: 20px;
 `;
 
+// Labels
 const Title = styled.Text`
   font-size: 16px;
   color: #b8bece;
   font-weight: 500;
 `;
 
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+`;
+
 const Name = styled.Text`
   font-size: 20px;
   color: #3c4560;
   font-weight: bold;
+`;
+// Images
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
